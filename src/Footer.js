73796b237e585f17f05b7eef38dbb605eb03a287
Harsh <harshvardhan.mec18@itbhu.ac.in>
2021-05-12 11:28:28 +0530

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+      <h1>
+        footer is working
+      </h1>
+    </div>
+  )
+};
+
+export default footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <h1>
+        header is working
+      </h1>
+    </div>
+  )
+};
+
+export default header;
